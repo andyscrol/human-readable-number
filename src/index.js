0535693192e4e49bd5b2b192a5c734f7d6009883
Alexey Larin <alexey_larin@hotmail.com>
2023-01-06 00:53:12 +0300

    init: start writing solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function toReadable (number) {
-  
+  let zeroToNineteen = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let twentyToNinety = ['','','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 }
